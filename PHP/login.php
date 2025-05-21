ea50152a13f8bc7cf1bf8bdf08c1a2e6828a9085
xqlo <soumeiyosinaga0129@icloud.com>
2025-05-21 14:52:30 +0900

    reset.cssの追加

--- PHP/login.php
+++ PHP/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ログイン | fitty.</title>
+        <link rel="stylesheet" href="../CSS/reset.css">
     <link rel="stylesheet" href="../CSS/login.css" />
 </head>
 
