ddde52e2e75734fb0b4cb55a7856f61658ac4c4d
n-38 <g6237n@gmail.com>
2025-05-21 13:18:31 +0900

    search作った

---
+++ PHP/search.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>探す</title>
+    <link rel="stylesheet" href="../CSS/search.css">
+</head>
+
+<body>
+    <h1>探す</h1>
+
+    <div class="form-container">
+    <div class="select_container">
+        <select name="brand">
+            <option value="" selected disabled hidden>ブランド</option>
+            <option value="1">HARE</option>
+            <option value="2">ONCILY</option>
+            <option value="3">LACOSTE</option>
+        </select>
+    </div>
+    <div class="select_container">
+        <select name="color">
+            <option value="" selected disabled hidden>カラー</option>
+            <option value="1">ブラック</option>
+            <option value="2">ホワイト</option>
+            <option value="3">レッド</option>
+        </select>
+    </div>
+    <div class="select_container">
+        <select name="genre">
+            <option value="" selected disabled hidden>ジャンル</option>
+            <option value="1">トップス</option>
+            <option value="2">パンツ</option>
+            <option value="3">シューズ</option>
+        </select>
+    </div>
+    </div>
+    <button>探す</button>
+</body>
+
+</html>
