6fde18e80cae8f16ab48990023eccb49b62ff804
n-38 <g6237n@gmail.com>
2025-05-23 13:15:54 +0900

    規約作った

---
+++ PHP/overview.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fitty. | 会社概要</title>
+    <link rel="stylesheet" href="../CSS/reset.css">
+    <link rel="stylesheet" href="../CSS/common.css">
+    <link rel="stylesheet" href="../CSS/overview.css">
+</head>
+<body>
+<main>
+    <h1>会社概要</h1>
+    <p>会社名：株式会社fitty.（フィッティー）</p>
+    <p>設立：2025年5月23日</p>
+    <p>所在地：東京都〇〇区〇〇1-1-11</p>
+    <p>代表者：代表取締役 Nguyễn Thị Minh Anh-Lê <br>(グエン・ティ・ミン・アイン＝レー)</p>
+    <img src="./img/ceo.jpeg" alt="ceo">
+    <p>資本金：1,000万円</p>
+
+</main>
+</body>
+</html>
