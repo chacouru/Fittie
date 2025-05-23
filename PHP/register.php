f40227ac82ec671b4b84e36494150ec4f21db814
xqlo <soumeiyosinaga0129@icloud.com>
2025-05-23 10:54:57 +0900

    header修正

--- PHP/register.php
+++ PHP/register.php
@@ -29,29 +29,31 @@
     </div>
     <div class="header_space"></div>
     <!-- headerここまで -->
-    <div class="login_container">
-        <h1>新規会員登録</h1>
-        <form action="register.php" method="POST">
-            <div class="form_group">
-                <label for="name">お名前</label>
-                <input type="text" id="name" name="name" required>
-            </div>
-            <div class="form_group">
-                <label for="email">メールアドレス</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form_group">
-                <label for="password">パスワード</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form_group">
-                <label for="confirm_password">パスワード（確認）</label>
-                <input type="password" id="confirm_password" name="confirm_password" required />
-            </div>
-            <button type="submit">登録する</button>
-        </form>
-        <p><a href="./login.php">▶ すでにアカウントをお持ちの方はこちら</a></p>
-    </div>
+    <main>
+        <div class="login_container">
+            <h1>新規会員登録</h1>
+            <form action="register.php" method="POST">
+                <div class="form_group">
+                    <label for="name">お名前</label>
+                    <input type="text" id="name" name="name" required>
+                </div>
+                <div class="form_group">
+                    <label for="email">メールアドレス</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form_group">
+                    <label for="password">パスワード</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <div class="form_group">
+                    <label for="confirm_password">パスワード（確認）</label>
+                    <input type="password" id="confirm_password" name="confirm_password" required />
+                </div>
+                <button type="submit">登録する</button>
+            </form>
+            <p><a href="./login.php">▶ すでにアカウントをお持ちの方はこちら</a></p>
+        </div>
+    </main>
 
     <!-- footer -->
     <footer class="footer">
