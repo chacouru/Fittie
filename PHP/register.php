3f006d1fd4019a536b3f486571257abdad62c514
xqlo <soumeiyosinaga0129@icloud.com>
2025-05-27 13:42:33 +0900

    新規登録のcss追加

--- PHP/register.php
+++ PHP/register.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fitty. | 新規登録</title>
+    <link rel="stylesheet" href="../CSS/register.css">
+</head>
 <body>
-<h2>新規登録</h2>
-<form method="POST" action="login_function/register_handler.php">
-    <input type="text" name="name" required placeholder="名前"><br>
-    <input type="email" name="email" required placeholder="メール"><br>
-    <input type="password" name="password" required placeholder="パスワード"><br>
-    <input type="text" name="address" placeholder="住所"><br>
-    <input type="text" name="phone" placeholder="電話番号"><br>
-    <button type="submit">登録</button>
-</form>
+    <main>
+        <h2>新規登録</h2>
+        <form method="POST" action="login_function/register_handler.php">
+            <input type="text" name="name" required placeholder="名前">
+            <input type="email" name="email" required placeholder="メール">
+            <input type="password" name="password" required placeholder="パスワード">
+            <input type="text" name="address" placeholder="住所">
+            <input type="text" name="phone" placeholder="電話番号">
+            <button type="submit">登録</button>
+        </form>
+    </main>
 </body>
 </html>
