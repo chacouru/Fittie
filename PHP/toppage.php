28dc56e41635a38b300980d70ab6c9f25c8c3807
xqlo <soumeiyosinaga0129@icloud.com>
2025-05-28 11:44:44 +0900

    ログイン後リンク変更

--- PHP/toppage.php
+++ PHP/toppage.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/login_function/session.php';
 if (isset($_SESSION['user_id'])) {
     echo "ログイン中：ユーザーID " . $_SESSION['user_id'];
 } else {
-    echo '<a href="login.php">ログイン</a> または <a href="register.php">登録</a>';
+    echo '<a href="index.php">ログイン</a> または <a href="register.php">登録</a>';
 }?>
 <!DOCTYPE html>
 <html lang="en">
